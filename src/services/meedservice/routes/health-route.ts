dd57ab82da0a8a53ab50a099c81226ba1f4c4db6
Shahwar Maiwandi <shahwar.maiwandi@gmail.com>
2020-04-10 13:04:00 -0700

    added country specific health message with server id to identify servers

--- src/services/meedservice/routes/health-route.ts
+++ src/services/meedservice/routes/health-route.ts
@@ -1,12 +1,13 @@
 import { NextFunction, Response, Request } from 'express';
 import config from '../../../config/config';
+const random = Math.floor(Math.random() * 10000);
 
 export const healthRoute = {
   path: 'v1.0.0/health',
   method: 'get',
   handler: [
     async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-      res.status(200).send(`${config.app.title} is healthy!`);
+      res.status(200).send(`${config.app.title} mx server ${random} is healthy!`);
     }
   ]
 };
